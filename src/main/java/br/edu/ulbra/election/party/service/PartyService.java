215ff160ac8c6ae42008bd8b69c669cfdc7f31dd
Bruno Moraes <bruno.delcio@ulbra.inf.br>
2018-11-02 18:09:07 -0300

    Update Party
    Update Party

--- src/main/java/br/edu/ulbra/election/party/service/PartyService.java
+++ src/main/java/br/edu/ulbra/election/party/service/PartyService.java
@@ -6,7 +6,6 @@ import br.edu.ulbra.election.party.input.v1.PartyInput;
 import br.edu.ulbra.election.party.model.Party;
 import br.edu.ulbra.election.party.output.v1.GenericOutput;
 import br.edu.ulbra.election.party.output.v1.PartyOutput;
-import br.edu.ulbra.election.party.repository.PartyRepository;
 import org.apache.commons.lang.StringUtils;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
